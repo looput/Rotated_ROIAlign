edf2bcfbd8580b90dcaae3cb92546b719c995a35
looput <41996180+looput@users.noreply.github.com>
2019-10-13 14:28:54 +0800

    Delete vision.cpp

--- vision.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-#include "ROIAlignRotated.h"
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("roi_align_rotated_forward",&ROIAlignRotated_forward,"ROIAlignRotated_forward");
-  m.def("roi_align_rotated_backward",&ROIAlignRotated_backward,"ROIAlignRotated_backward");
-}
